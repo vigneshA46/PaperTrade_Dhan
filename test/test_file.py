53c0fe1d1fd51f51de287cb3cc4b608d472de823
dreaminalgodevelopmement-cmyk <dreaminalgodevelopmement@gmail.com>
2026-03-20 10:42:46 +0530

    things done for logger

--- test/test_file.py
+++ test/test_file.py
@@ -1,16 +1,60 @@
-import asyncio
-from dhanhq import marketfeed
-from dhan_token import get_access_token
-import os
-from find_security import load_fno_master
-from find_security import find_option_security
+import requests
 
 
-fno = load_fno_master()
 
-print(fno)
+COMMON_ID = "1fff432a-0411-40ff-aefd-c0b0026d5a6d"
+SYMBOL = "NIFTY"
+LOTSIZE = 65
+TRADE_LOG_URL = "https://dreaminalgo-backend-production.up.railway.app/api/paperlogger/event"
 
-data = find_option_security(fno ,26300, 'CE','2026-03-02','NIFTY')
+def log_trade_event(
+    
+    event_type,   # ENTRY / EXIT
+    leg_name,
+    token,
+    symbol,
+    side,
+    lot,
+    price,
+    reason
+    ):
+    payload = {
+        "run_id": COMMON_ID,
+        "strategy_id": COMMON_ID,
 
-print(data['SECURITY_ID'])
- 
+        "trade_id": COMMON_ID,         # 🔥 VERY IMPORTANT
+        "event_type": event_type,     # ENTRY / EXIT
+
+        "leg_name": leg_name,
+        "token": int(token),
+        "symbol": symbol,
+
+        "side": side,
+        "lots": lot,
+        "quantity": lot * LOTSIZE,
+
+        "price": price,
+
+        "reason": reason,
+        "deployed_by": COMMON_ID
+    }
+
+    try:
+        requests.post(TRADE_LOG_URL, json=payload, timeout=3)
+    except Exception as e:
+        print("TRADE EVENT LOG ERROR:", e)
+
+
+
+
+log_trade_event(
+            
+            event_type="EXIT",
+            leg_name="CE",
+            token=12345,
+            symbol=SYMBOL,
+            side="SELL",
+            lot=1,
+            price=243.46,
+            reason="Below Mark"
+                )
