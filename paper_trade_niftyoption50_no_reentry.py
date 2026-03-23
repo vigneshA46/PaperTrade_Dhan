674af6ccb3a39d826d6fcea65e7021952af9add2
dreaminalgodevelopmement-cmyk <dreaminalgodevelopmement@gmail.com>
2026-03-23 08:57:51 +0530

    vvsdv

--- paper_trade_niftyoption50_no_reentry.py
+++ paper_trade_niftyoption50_no_reentry.py
@@ -409,7 +409,7 @@ def handle_leg(name, token, candle, state, ltp):
 
 
         state["trading_disabled"] = True
-        return
+            return
 
     # =========================
     # STOP TRADING
