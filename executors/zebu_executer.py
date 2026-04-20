9cce7825267bea5708273cacfd71fb2529696a8a
Janani <jananireddy33@gmail.com>
2026-04-20 20:42:23 +0530

    executer done

--- executors/zebu_executer.py
+++ executors/zebu_executer.py
@@ -1,4 +1,10 @@
 from brokers.zebuclient import ZebuClient
+import requests
+from datetime import datetime
+import uuid
+import asyncio
+
+API_URL = "https://algoapi.dreamintraders.in/api/realtradegroups"
 
 async def zebu_order(user, signal):
     try:
@@ -50,8 +56,38 @@ async def zebu_order(user, signal):
         )
 
         print(response)
+        if response:
 
-        print(f"✅ ZEBU order success {user['user_id']}")
+            print(f"✅ ZEBU order success {user['user_id']}")
+
+            payload = {
+                "user_id": user["user_id"],
+                "strategy_id": signal["strategy_id"],
+                "broker_id": user["broker_account_id"], 
+                "trade_id": str(uuid.uuid4()),
+                "trade_date": datetime.now().strftime("%Y-%m-%d"),
+                "event_type": signal["event_type"],
+                "leg_name": signal["leg_name"],
+                "symbol": signal["symbol"],
+                "side": signal["side"],
+                "quantity": qty,
+                "price": signal["price"],
+                "pnl": signal["pnl"],
+                "cum_pnl": signal["cum_pnl"],
+                "reason": signal["reason"]
+            }
+
+            #print("Payload:",payload)
+
+            response = await asyncio.to_thread(requests.post, API_URL, json=payload)
+
+            #print("📥 STATUS:", response.status_code)
+            #print("📥 RESPONSE:", response.text)
+
+            if response.status_code == 201:
+                print("Trade logged successfully")
+            else:
+                print(f"API failed: {response.text}")
 
     except Exception as e:
         print(f"❌ ZEBU failed {user['user_id']}: {e}")
