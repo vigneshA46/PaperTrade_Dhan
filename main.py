31bce434388b2b2e7086c0707a1b51151203bb7d
Janani <jananireddy33@gmail.com>
2026-04-21 10:14:41 +0530

    eventloop err done

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from dispatcher import publish
 import paper_trade_niftyoption50_no_reentry as strategy1
-import paper_trade_niftyoption50_reentry as strategy2
-import paper_trade_niftyoption35_reentry as strategy3
-import paper_trade_niftyoption35_reentry_point as strategy4
-import paper_trade_niftyoption50_reentry_point as strategy5
+#import paper_trade_niftyoption50_reentry as strategy2
+#import paper_trade_niftyoption35_reentry as strategy3
+#import paper_trade_niftyoption35_reentry_point as strategy4
+#import paper_trade_niftyoption50_reentry_point as strategy5
 #import range_breakout_selling as strategy5
-import delta_option_buying as strategy6
+#import delta_option_buying as strategy6
 #import bank_nifty_option_buying as strategy7
-import paper_trade_niftyoption8_no_reentry as strategy8
+#import paper_trade_niftyoption8_no_reentry as strategy8
 from dhanhq import marketfeed
 from dhanhq import dhanhq
 from dhan_token import get_access_token
@@ -17,7 +17,7 @@ import os
 # collect all tokens
 ALL_TOKENS = set()
 ALL_TOKENS.update(strategy1.TOKENS)
-ALL_TOKENS.update(strategy6.TOKENS)
+#ALL_TOKENS.update(strategy6.TOKENS)
 #ALL_TOKENS.update(strategy7.TOKENS)
 #ALL_TOKENS.update(strategy5.TOKENS)
 
