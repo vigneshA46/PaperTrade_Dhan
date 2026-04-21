788b348b75fbdc221da11ad5d8c2cb590d7b2720
Janani <jananireddy33@gmail.com>
2026-04-21 09:56:28 +0530

    eventloop err done

--- signal_emitter.py
+++ signal_emitter.py
@@ -1,6 +1,5 @@
 import asyncio
 from signal_manager import process_signal
 
-def emit_signal(signal):
-    loop = asyncio.get_event_loop()
-    loop.create_task(process_signal(signal))
+async def emit_signal(signal):
+    await process_signal(signal)
