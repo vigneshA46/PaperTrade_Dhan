258438bedcecb2bc47ea836aa5d00e453d9c2bfb
Janani <jananireddy33@gmail.com>
2026-05-18 10:13:20 +0530

    done

--- delta_option_buying.py
+++ delta_option_buying.py
@@ -149,7 +149,7 @@ def build_payload(name, side, token , reason,event_type,ltp,pnl,cum_pnl,lot,user
         symbol = f"NIFTY{day}{month}{year}{ce_strike}{name}"
     else:
         symbol = f"NIFTY{day}{month}{year}{pe_strike}{name}"
-        
+
     expiry = expiry_date.strftime("%Y-%m-%d")
 
     return {
@@ -562,21 +562,21 @@ best_ce = None
 best_pe = None
 
 
-ce_strike = CE_STRIKE
-pe_strike = PE_STRIKE
+ce_strike = str(CE_STRIKE)
+pe_strike = str(PE_STRIKE)
 
 
 finder=FindInstrument()
 
-ce_row = find_option_security(fno_df, ce_strike, "CE", today, "NIFTY")
-pe_row = find_option_security(fno_df, pe_strike, "PE", today, "NIFTY")
+ce_row = find_option_security(fno_df, CE_STRIKE, "CE", today, "NIFTY")
+pe_row = find_option_security(fno_df, PE_STRIKE, "PE", today, "NIFTY")
 
 CE_ID = str(ce_row["SECURITY_ID"])
 PE_ID = str(pe_row["SECURITY_ID"])
 
 
 AngelCE = finder.get_option("NIFTY" , int(ce_strike) , "CE")
-AngelPE = finder.get_option("NIFTY" , int(ce_strike) , "PE")
+AngelPE = finder.get_option("NIFTY" , int(pe_strike) , "PE")
 
 print("angel tokens" , AngelCE , AngelPE)
 
