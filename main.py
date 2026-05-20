e2dbeaea14ebe9df5df4557f4fdba2237545f98c
Janani <jananireddy33@gmail.com>
2026-05-20 12:24:16 +0530

    done

--- main.py
+++ main.py
@@ -69,20 +69,28 @@ def on_message(msg):
         now = datetime.now(ist)
 
         if not rb_buying and now.hour == 9 and now.minute >= 31:
-  
+
             rb_buying = True
+
             try:
 
                 print("Starting Range Breakout Buying")
 
                 import range_breakout_buying as strategy12
+
+                print("12 LOADED")
+
                 import range_breakout_buying_cum as strategy13
+
+                print("13 LOADED")
+
                 import range_breakout_buying_points as strategy14
+
+                print("14 LOADED")
 
             except Exception as e:
 
                 print("RB BUYING ERROR:", e)
-
         if not rb_started and now.hour >= 10 and now.minute >= 1:
 
                 try:
