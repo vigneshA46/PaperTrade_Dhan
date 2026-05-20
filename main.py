5d33907a711c1568dfb0da37655134c3b197ce51
Janani <jananireddy33@gmail.com>
2026-05-20 12:49:04 +0530

    done

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def on_message(msg):
         ist = pytz.timezone("Asia/Kolkata")
         now = datetime.now(ist)
 
-        if now.hour == 9 and now.minute >= 31:
+        if now.hour >= 9 and now.minute >= 31:
 
             print("Starting Range Breakout Buying")
 
